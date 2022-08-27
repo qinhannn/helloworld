f353d9aebddd23929215b66c2b80efeaab45c640
An Nguyen <An Nguyen@DESKTOP-ULRTD14>
2022-08-26 22:22:54 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace helloworld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string stg = "Hello, World! ";
+            Console.WriteLine(stg);
+
+            Console.WriteLine("Enter Username");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Enter user's age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Username is: " + userName + " and the age is: " + age);
+        }
+    }
+}
